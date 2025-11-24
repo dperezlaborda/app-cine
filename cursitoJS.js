c8769756d0694fb7d48fcbe7681e86cb05faafed
dperezlaborda <doloresperezlaborda@gmail.com>
2025-11-24 19:07:18 -0300

    Se agrega logica de salas y disponibilidad de asientos - se implementan metodos - se agrega flujo de compra - se agregan estilos necesarios

--- cursitoJS.js
+++ cursitoJS.js
@@ -1,104 +1,307 @@
-const peliculas = [
-    "El Viaje de Chihiro",
-    "El Señor de los Anillos",
-    "El Padrino",
-    "Interstellar",
-    "Superman",
-    "Superman II",
-]
+const movies = [
+    {
+        title: "El Viaje de Chihiro",
+        image: "https://www.themoviedb.org/t/p/w1280/laXrmaTRuroArSPfsGlvTbeWxVA.jpg"
+    },
+    {
+        title: "El Señor de los Anillos",
+        image: "https://www.themoviedb.org/t/p/w1280/z632eZtXaw76ZE5mMMGOBXCpm1T.jpg"
+    },
+    {
+        title: "El Padrino",
+        image: "https://www.themoviedb.org/t/p/w1280/ApiEfzSkrqS4m1L5a2GwWXzIiAs.jpg"
+    },
+    {
+        title: "Drácula",
+        image: "https://www.themoviedb.org/t/p/w1280/ff7fyDdtXM2wu9jsm9gOMYtpvOl.jpg"
+    },
+    {
+        title: "Superman",
+        image: "https://www.themoviedb.org/t/p/w1280/fvUJb08yatV2b3NUSwuYdQKYoFd.jpg"
+    },
+    {
+        title: "Depredador",
+        image: "https://www.themoviedb.org/t/p/w1280/jl5Y5p2pbhgRwREWbll2RbciG5Z.jpg"
+    }
+];
 
-const horarios = [
+const showtimes = [
     "12:00",
     "15:00",
     "18:00",
     "19:00",
 ]
 
-let asientosSeleccionados = [];
-let asientosBloqueados = [];
-let cantidadEntradas = 0;
-let total = 0;
+const userSelecction = {
+    movie: null,
+    showTime: null,
+    tickets: 0,
+    seats: [],
+};
 
-function elegirPelicula() {
-    let pelicula = prompt("Elija una pelicula: \n" + peliculas.join("\n"));
-    while(!peliculas.includes(pelicula)) {
-        pelicula = prompt("Pelicula no disponible. Elija una pelicula: \n" + peliculas.join("\n"));
+let screeningRooms = [];
+
+class ScreeningRoom  {
+    constructor(id, movie, showTime, capacidad) {
+        this.id = id;
+        this.movie = movie;
+        this.showTime = showTime;
+        this.availableSeats = capacidad;
     }
-    return pelicula;
 }
 
-function elegirHorario() {
-    let horario = prompt("Elija un horario: \n" + horarios.join("\n"));
-    while(!horarios.includes(horario)) {
-        horario = prompt("Horario no disponible. Elija un horario: \n" + horarios.join("\n"));
-    }
-    return horario;
+function loadRoomsFromStorage() {
+    screeningRooms = JSON.parse(localStorage.getItem('salas')) || [];
 }
 
-function elegirCantidadEntradas() {
-    cantidadEntradas = parseInt(prompt("¿Cuántas entradas desea comprar? (1-5): "));
-    while(cantidadEntradas < 1 || cantidadEntradas > 5 || isNaN(cantidadEntradas)) {
-        cantidadEntradas = parseInt(prompt("Cantidad inválida. ¿Cuántas entradas desea comprar? (1-5): "));
-    }
-    return cantidadEntradas;
+function saveRoomsToStorage(rooms) {
+    localStorage.setItem('salas', JSON.stringify(rooms));
 }
 
-function elegirAsiento() {
-    //TO-DO: VALIDAR QUE EL ASIENTO SE GUARDE EN ARRAY POR PELICULA Y HORARIO
-    let asientosOcupados = [];
+function createScreeningRoom(movie, showTime) {
+    let room = screeningRooms.find(room => room.movie === movie && room.showTime === showTime);
+    if (!room) {
+        const newId = screeningRooms.length + 1;
+        room = new ScreeningRoom (newId, movie, showTime, 8);
+        screeningRooms.push(room);
+        saveRoomsToStorage(screeningRooms)
+    }
+    return room;
+}
 
-    for(let i = 0; i < cantidadEntradas; i++) {
-        let asiento = parseInt(prompt("Seleccione sus asientos (1-100): "));
+function showMoviesContainer() {
+   const moviesContainer = document.getElementById("movies-container");
+   
+   movies.forEach(movie => {
+       const movieCard = document.createElement("article");
+       movieCard.classList.add("movie-card");
 
-        while(asiento < 1 || asiento > 100 || isNaN(asiento)) {
-            asiento = parseInt(prompt("Asiento inválido. Seleccione sus asientos (1-100): "));
+       const image = document.createElement("img");
+       image.classList.add("movie-card-image");
+       image.src = movie.image;
+       image.alt = movie.title
+
+       const title = document.createElement("h3");
+       title.innerText = movie.title;
+
+       const buyBttn = document.createElement("button");
+       buyBttn.innerText = "Seleccionar Película";
+       buyBttn.classList.add("movie-card-bttn");
+
+       buyBttn.onclick = () => {
+           selectMovie(movie.title);
+       };
+
+       movieCard.appendChild(image);
+       movieCard.appendChild(title);
+       movieCard.appendChild(buyBttn);
+       moviesContainer.appendChild(movieCard);
+   });
+}
+
+function selectMovie(movie) {
+    const selectedMovie = document.getElementById("selectedObject");
+    selectedMovie.classList.add("selected-movie");
+    userSelecction.movie = movie;
+
+    const title = document.createElement("h3");
+    title.innerText = `Película seleccionada: ${movie}`;
+
+    selectedMovie.appendChild(title)
+
+    hideElements();
+    showSchedule();
+}
+
+function showSchedule() {
+    const selectedShowTime= document.getElementById("selectedShowTime");
+    selectedShowTime.classList.add("selected-showtime");
+    
+    showtimes.forEach(showTime => {
+        const scheduleCard = document.createElement("div");
+        scheduleCard.classList.add("showtime-card");
+
+        const title = document.createElement("h3");
+        title.innerText = showTime;
+
+        const selectBttn = document.createElement("button");
+        selectBttn.innerText = "Seleccionar";
+        selectBttn.classList.add("select-horario-bttn");
+
+        selectBttn.onclick = () => {
+            selectSchedule(showTime);
         }
 
-        while(asientosBloqueados.includes(asiento)) {
-            asiento = parseInt(prompt(`El asiento ${asiento} ya está ocupado. Seleccione otro asiento (1-100): `));
+        scheduleCard.appendChild(title);
+        scheduleCard.appendChild(selectBttn);
+        selectedShowTime.appendChild(scheduleCard);
+    });
+};
+
+function selectSchedule(showTime) {
+    const selectedSchedule = document.getElementById("selectedObject");
+    userSelecction.showTime = showTime;
+
+    const title = document.createElement("h3");
+    title.innerText = `Horario seleccionado: ${showTime}`;
+    selectedSchedule.appendChild(title);
+
+    hideElements();
+    selectSeats();
+}
+
+function selectSeats() {
+    const selectedSeatsContainer = document.getElementById("selectedSeats");
+    
+    const seatsCard = document.createElement("div");
+    seatsCard.classList.add("seats-card");
+
+    const title = document.createElement("h3");
+    title.innerText = "Selección de Butacas";
+
+    const currentRoom = createScreeningRoom(userSelecction.movie, userSelecction.showTime);
+
+    seatsCard.appendChild(title);
+    selectedSeatsContainer.appendChild(seatsCard);
+
+    showGridSeats(seatsCard, currentRoom);
+};
+
+function showGridSeats(container, currentRoom) {
+    const gridContainer = document.createElement("div");
+    gridContainer.classList.add("grid-seats-container");
+
+    const info = document.createElement("p");
+    info.innerText = `Seleccione la cantidad de butacas, disponibles: ${currentRoom.availableSeats}`;
+    
+    const selectedSeats = [];
+
+    if (currentRoom.availableSeats > 0) {
+        for (let i = 1; i <= currentRoom.availableSeats; i++) {
+            const seatBttn = document.createElement("button");
+            seatBttn.classList.add("seat-bttn");
+            seatBttn.innerText = i;
+
+            seatBttn.onclick = () => {
+                const index = selectedSeats.indexOf(i);
+            
+                if (index === -1){
+                    selectedSeats.push(i);
+                    seatBttn.classList.add("selected-seat");
+                } else {
+                    selectedSeats.splice(index, 1);
+                    seatBttn.classList.remove("selected-seat");
+                }
+            }
+
+            gridContainer.appendChild(seatBttn);
         }
 
-        while(asientosOcupados.includes(asiento)) {
-            asiento = parseInt(prompt(`El asiento ${asiento} ya está ocupado. Seleccione otro asiento (1-100): `));
+        const confirmBttn = document.createElement("button");
+        confirmBttn.innerText = "Confirmar Compra";
+        confirmBttn.classList.add("confirm-bttn");
+
+        confirmBttn.onclick = () => {
+            if (selectedSeats.length > 0) {
+                userSelecction.tickets = selectedSeats.length;
+                userSelecction.seats = selectedSeats;
+                completePurchase(selectedSeats.length);
+            }else{
+                alert("Debe seleccionar al menos 1 butaca");
+            }
         }
 
-        asientosOcupados.push(asiento);
-
+        container.appendChild(info);
+        container.appendChild(gridContainer);
+        container.appendChild(confirmBttn);
+    }else{
+        const noSeats = document.createElement("p");
+        noSeats.innerText = "No hay butacas disponibles para este horario.";
+        container.appendChild(noSeats);
     }
-    return asientosOcupados;
+}
+
+function completePurchase(seatsCount) {
+    userSelecction.tickets = seatsCount;
+
+    const currentRoom = createScreeningRoom(userSelecction.movie, userSelecction.showTime);
+    currentRoom.availableSeats -= seatsCount;
+    saveRoomsToStorage(screeningRooms)
+
+    const newPurchase = {
+        movie: userSelecction.movie,
+        showTime: userSelecction.showTime,
+        tickets: userSelecction.tickets,
+        seats: userSelecction.seats
+    };
+
+    const store = JSON.parse(localStorage.getItem('compras')) || [];
+    store.push(newPurchase);
+    localStorage.setItem('compras', JSON.stringify(store));
+
+    const title = document.createElement("h3");
+    title.innerText = `Compra confirmada para pelicula ${userSelecction.movie} en el horario ${userSelecction.showTime} por ${seatsCount} butacas. ¡Gracias por su compra!`;
+
+    const buyAgainBttn = document.createElement("button");
+    buyAgainBttn.innerText = "Realizar otra compra";
+    buyAgainBttn.classList.add("buy-again-bttn");
+    buyAgainBttn.onclick = () => {
+        clearSelection();
+        showMoviesContainer();
+    }
+
+    selectedObject.appendChild(title);
+    selectedObject.appendChild(buyAgainBttn);
+    hideElements();
+}
+
+function clearSelection(){
+    userSelecction.movie = null;
+    userSelecction.showTime = null;
+    userSelecction.tickets = 0;
+    userSelecction.seats = [];
+
+    document.getElementById("selectedObject").innerHTML = '';
+    document.getElementById("selectedShowTime").innerHTML = '';
+    document.getElementById("selectedSeats").innerHTML = '';
+    document.getElementById("movies-container").innerHTML = '';
+}
+
+function hideElements() {
+    if(userSelecction.movie){
+        const moviesContainer = document.getElementById("movies-container");
+        moviesContainer.innerHTML = '';
+    }
+
+    if(userSelecction.showTime){
+        const selectedSchedule = document.getElementById("selectedShowTime");
+        selectedSchedule.innerHTML = '';
+    }
+
+    if(userSelecction.tickets > 0){
+        const selectedSeats = document.getElementById("selectedSeats");
+        selectedSeats.innerHTML = '';
+    }
 
 }
 
-function mostrarResumen(pelicula, horario, cantidad, asientos) {
-    let total = cantidad * 5500;
-    console.log("Resumen de su compra:");
-    console.log("Pelicula: " + pelicula);
-    console.log("Horario: " + horario);
-    console.log("Cantidad de entradas: " + cantidad);
-    console.log("Asientos: " + asientos);
-    console.log("Total a pagar: $" + total);
-
-    const modificacion = prompt("¿Desea modificar su compra? (si/no): ").toLowerCase();
-    const quiereModificar = modificacion === "si";
-
-    if(quiereModificar) {
-        console.log('Se modificará la compra.');
-        comprarEntradas();
+function showPreviousPurchase() {
+    const lastPurchaseDiv = document.getElementById("lastPurchase");
+    const storedPurchases = JSON.parse(localStorage.getItem('compras')) || [];
+    if (storedPurchases.length > 0) {
+        const lastPurchase = storedPurchases[storedPurchases.length - 1];
+        lastPurchaseDiv.innerHTML = `<h3>Última compra: ${lastPurchase.tickets} entradas para "${lastPurchase.movie}" a las ${lastPurchase.showTime}.</h3>`;
     }else {
-        console.log('no entra?');
-        asientosBloqueados = asientosBloqueados.concat(asientos);
-        console.log('Asientos bloqueados: ' + asientosBloqueados);
-        const resumen = `Compra confirmada:\nPelicula: ${pelicula}\nHorario: ${horario}\nCantidad de entradas: ${cantidad}\nAsientos: ${asientos}\nTotal a pagar: $${total}\n¡Disfrute la película!`;
-        console.log(resumen);
+        lastPurchaseDiv.innerHTML = `<h3>No hay compras realizadas aún.</h3>`;
     }
 }
 
-function comprarEntradas() {
-    const peliculaSeleccionada = elegirPelicula();
-    const horarioSeleccionado = elegirHorario();
-    const cantidadEntradas = elegirCantidadEntradas();
-    const asientosSeleccionados = elegirAsiento();
 
-    const resumen = mostrarResumen(peliculaSeleccionada, horarioSeleccionado, cantidadEntradas, asientosSeleccionados);
-    return resumen;
-}
+window.addEventListener('DOMContentLoaded', () => {
+    loadRoomsFromStorage();
+    showPreviousPurchase();
+    showMoviesContainer();
+});
+
+
+
